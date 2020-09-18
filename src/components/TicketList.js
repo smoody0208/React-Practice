8769a6e53f3c518057d1a43df2626e30b92f69d5
Spencer Moody <spencer.moody@outlook.com>
2020-09-18 13:23:46 -0700

    Add propTypes and update TicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,9 +1,36 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const masterTicketList = [
+  {
+    names: 'Thato and Haley',
+    location: '3A',
+    issue: 'Firebase won\'t save record. Halp.'
+  },
+  {
+    names: 'Sleater and Kinney',
+    location: '4B',
+    issue: 'Prop types are throwing an error.'
+  },
+  {
+    names: 'Imani & Jacob',
+    location: '9F',
+    issue: 'Child component isn\'t rendering.'
+  }
+];
+
 function TicketList(){
   return (
-    <Ticket />
+    <React.Fragment>
+      <Ticket
+        location="3A"
+        names="Thato and Haley"
+        issue="Firebase will not save record!"/>
+      <Ticket
+        location="4B"
+        names="Sleater and Kinney"
+        issue="Prop types are throwing an error."/>
+    </React.Fragment>
   );
 }
 
